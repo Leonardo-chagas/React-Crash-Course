be35f0d72d7cef6524e816a1db9d41d6bc020cb6
leonardo-chagas <leoschagas@hotmail.com>
2022-10-29 22:37:58 -0300

    completou 60% do tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Components/Header";
+import Tasks from "./Components/Tasks";
+import { useState } from 'react'
 
 function App() {
+  const [tasks, setTasks] = useState([
+     {
+        "id": 1,
+        "text": "Doctors Appointment",
+        "day": "May 5th at 2:30pm",
+        "reminder": true
+      },
+      {
+        "id": 2,
+        "text": "Meeting at School",
+        "day": "May 6th at 1:30pm",
+        "reminder": true
+      },
+      {
+        "id": 3,
+        "text": "Food Shopping",
+        "day": "May 7th at 12:30pm",
+        "reminder": false
+      }
+  ])
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header></Header>
+      {tasks.length > 0 ? (<Tasks 
+      tasks={tasks}
+      onDelete={deleteTask}></Tasks>) : ('No tasks to show')}
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+
+const Header = ({title}) => {
+
+  const onClick = () => {
+    console.log('click');
+  }
+
+  return (
+    <header className='header'>
+      <h1>{title}</h1>
+      <Button 
+      onClick={onClick}
+      color='green' 
+      text='Add'></Button>
+    </header>
+  )
+}
+
+Header.defaultProps = {
+  title: 'Task Tracker',
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+}
+
+export default Header
