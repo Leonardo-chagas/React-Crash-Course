79e03fbd78e94478e266e08508686b8d792fa9ef
leonardo-chagas <leoschagas@hotmail.com>
2022-10-30 22:18:11 -0300

    concluiu o tutorial

--- src/App.js
+++ src/App.js
@@ -1,40 +1,95 @@
 import Header from "./Components/Header";
 import Tasks from "./Components/Tasks";
-import { useState } from 'react'
+import AddTask from "./Components/AddTask";
+import Footer from "./Components/Footer";
+import About from "./Components/About";
+import { useState, useEffect } from 'react'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 function App() {
-  const [tasks, setTasks] = useState([
-     {
-        "id": 1,
-        "text": "Doctors Appointment",
-        "day": "May 5th at 2:30pm",
-        "reminder": true
-      },
-      {
-        "id": 2,
-        "text": "Meeting at School",
-        "day": "May 6th at 1:30pm",
-        "reminder": true
-      },
-      {
-        "id": 3,
-        "text": "Food Shopping",
-        "day": "May 7th at 12:30pm",
-        "reminder": false
-      }
-  ])
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [tasks, setTasks] = useState([])
 
-  const deleteTask = (id) => {
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+
+    getTasks()
+  }, [])
+
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+    return data
+  }
+
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+    return data
+  }
+
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers:{
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    })
+
+    const data = await res.json()
+    setTasks([...tasks, data])
+
+    /* const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = {id, ...task};
+
+    setTasks([...tasks, newTask]); */
+  }
+
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {method: 'DELETE'})
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updTask)
+    })
+
+    const data = await res.json()
+
+    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: data.reminder} : task));
+  }
+
   return (
-    <div className="container">
-      <Header></Header>
-      {tasks.length > 0 ? (<Tasks 
-      tasks={tasks}
-      onDelete={deleteTask}></Tasks>) : ('No tasks to show')}
-    </div>
+    <Router>
+      <div className="container">
+        <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}></Header>
+        <Routes>
+          <Route path="/" element={
+            <>
+              {showAddTask && <AddTask onAdd={addTask}></AddTask>}
+              {tasks.length > 0 ? (<Tasks 
+              tasks={tasks}
+              onDelete={deleteTask}
+              onToggle={toggleReminder}></Tasks>) : ('No tasks to show')}
+            </>
+          }></Route>
+          <Route path="/about" element={<About />}></Route>
+        </Routes>
+        <Footer></Footer>
+      </div>
+    </Router>
   )
 }
 
--- src/Components/Tasks.js
+++ src/Components/Tasks.js
@@ -2,11 +2,11 @@ import React from 'react'
 import Task from './Task'
 
 
-const Tasks = ({tasks, onDelete}) => {
+const Tasks = ({tasks, onDelete, onToggle}) => {
   return (
     <div>
-      {tasks.map((task) => (
-        <Task key={task.id} task={task} onDelete={onDelete}></Task>
+      {tasks.map((task, index) => (
+        <Task key={index} task={task} onDelete={onDelete} onToggle={onToggle}></Task>
       ))}
     </div>
   )
